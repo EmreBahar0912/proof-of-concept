7116e67df0f2cac9562bf48312eb693feec009e3
EmreBahar0912 <e.bahar906@gmail.com>
2026-06-03 14:03:25 +0200

    Content First (dynamic version) #5

--- server.js
+++ server.js
@@ -14,9 +14,20 @@ app.engine('liquid', engine.express());
 app.set('views', './views')
 
 app.get('/', async function (request, response) {
-   // Render index.liquid uit de Views map
-   // Geef hier eventueel data aan mee
-   response.render('index.liquid')
+
+    const pokemonResponse = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10');
+    const pokemonData = await pokemonResponse.json();
+    
+    const allPokemon = pokemonData.results.map((p) => {
+        const id = p.url.split('/').filter(Boolean).pop();
+        return {
+            name: p.name,
+            id: id,
+            image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
+        }
+    });
+
+   response.render('index.liquid', { pokemons: allPokemon })
 })
 
 app.set('port', process.env.PORT || 8000)
