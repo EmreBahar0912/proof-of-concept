b87708da19ef75c9921f75ed578e84483c7a4e9e
EmreBahar0912 <e.bahar906@gmail.com>
2026-06-10 13:56:19 +0200

    #10 #20 #5 #12  Heart-icon animation added for a Pleasureable UI & JavaScript refactor searchbar-form id

--- public/main.js
+++ public/main.js
@@ -1,11 +1,19 @@
-document.getElementById('searchbar-form').addEventListener('submit', async (e) => {
-  e.preventDefault();
-  const val = document.getElementById('searchbar').value.toLowerCase();
-  if (!val) return;
+const form = document.getElementById('searchbar-form');
+if (form) {
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        const val = document.getElementById('searchbar').value.toLowerCase();
+        if (!val) return;
 
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${val}`);
-  if (!res.ok) return;
-  const data = await res.json();
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${val}`);
+        if (!res.ok) return;
+        const data = await res.json();
 
-  window.location.href = `/pokemon/${data.id}`;
+        window.location.href = `/pokemon/${data.id}`;
+    });
+}
+
+
+document.querySelector('.heart').addEventListener('click', function() {
+    this.closest('.heart-icon-container').classList.toggle('on');
 });
